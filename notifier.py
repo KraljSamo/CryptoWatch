ca9d943b5ac9b1328c3b965a1da4688fc29e284e
KraljSamo <samokralj95@gmail.com>
2019-09-09 19:19:10 +0200

    basic_framework_done

--- notifier.py
+++ notifier.py
@@ -4,6 +4,7 @@ import smtplib, ssl
 from dotenv import load_dotenv
 
 from email.mime.text import MIMEText
+import datetime
 
 class Notifier:
 
@@ -16,7 +17,7 @@ class Notifier:
         self.port = port
 
 
-    def send_email(self, message, subject):
+    def send_email(self, subject, message):
 
         with smtplib.SMTP(host=self.smtp, port=self.port) as server:
             
@@ -29,7 +30,28 @@ class Notifier:
             msg['To'] = self.target
 
             server.send_message(msg)
+    
+    def create_update_message(self, notifications):
+        
+        sorted_by_importance = sorted(notifications, reverse=True)
+
+        subject = '[CRYPTO] ' + ' '.join((str(e[1]) + ' : ' + str(round((e[0] - 1)*100, 2)) + '%' for e in sorted_by_importance[:3]))
+
+        message = """"""
+
+        for change, coin, date_past, price_past, date_now, price_now in sorted_by_importance:
+
+            message += '\n'
+            message += f'{coin} -> PRICE CHANGE -> {change} in the last {date_now - date_past}\n \n'
+            message += f'{coin} -- {date_past} -- {price_past} \n' 
+            message += f'{coin} -- {date_now} -- {price_now} \n \n' 
+
+        print(subject)
+        print(message)
+
+        self.send_email(subject, message)
 
 if __name__ == '__main__':
     notifier = Notifier('smtp-mail.outlook.com', 587)
-    notifier.send_email('This is a test email!', 'Test email - do not delete')
+    # notifier.send_email('This is a test email!', 'Test email - do not delete')
+    notifier.create_update_message([[1.1, 'bitcoin', datetime.datetime.now(), 10000, datetime.datetime.now(), 11000]])
--- watcher.py
+++ watcher.py
@@ -9,6 +9,12 @@ import time
 
 from notifier import Notifier
 
+def datetime_to_string(dt):
+    return dt.strftime("%m/%d/%Y, %H:%M:%S")
+
+def string_to_datetime(st):
+    return datetime.datetime.strptime(st, "%m/%d/%Y, %H:%M:%S")
+
 
 class Watcher():
 
@@ -35,7 +41,7 @@ class Watcher():
         coin_data = self.call_coingecko_api(url, params)
 
         ids = [coin['id'] for coin in coin_data]
-        self.top100 = ids
+        self.coins = ids
         
         return ids
     
@@ -63,21 +69,53 @@ class Watcher():
 
         current_time = datetime.datetime.now()
         current_time = current_time.replace(second=0, microsecond=0)
+        current_time_string = datetime_to_string(current_time)
 
         for coin, value in prices.items():
-            self.crypto_data[coin].append((current_time, value['usd']))
+            self.crypto_data[coin].append((current_time_string, value['usd']))
             print(coin, self.crypto_data[coin])
 
         return prices
+    
+    def check_coin_anomalies(self):
+        
+        notifications = []
 
-    def check_prices(self):
-        pass
+        for coin, price_data in self.crypto_data.items():
+            last_date, last_price = price_data[-1]
+            last_date = string_to_datetime(last_date)
+            
+            index = len(price_data) - 2
+            while index >= 0:
+                date, value = price_data[index]
+                date = string_to_datetime(date)
+                if last_date - date > datetime.timedelta(hours=2):
+                    break
+                if value * (1 + self.price_change) < last_price:
+                    notifications.append((last_price/value, coin, date, value, last_date, last_price))
+                    break
+                
+                index -= 1
+        
+        return notifications
 
     def run(self):
+
+        save_cycle = 0
         
         while True:
             try:
                 self.update_prices()
+                notifications = self.check_coin_anomalies()
+
+                if len(notifications) > 0:
+                    self.notifier.create_update_message(notifications)
+                
+                if save_cycle == 0:
+                    self.save()
+                
+                save_cycle = (save_cycle + 1) % 10
+
             except KeyboardInterrupt:
                 break
             except Exception as e:
